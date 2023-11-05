08cecaee8af3f82417f62b0b88a9d64bab320417
Nouman Syed <syednouman2114@gmail.com>
2023-11-05 18:20:57 -0500

    Initial Commit

---
+++ GithubUserSearch/GithubUserSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubUserSearchApp.swift
+//  GithubUserSearch
+//
+//  Created by Nouman Syed on 11/5/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubUserSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
